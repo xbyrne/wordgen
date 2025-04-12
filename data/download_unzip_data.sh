52ec7df4d624f1753beaea532dde1bb37dd6b64a
Xander Byrne <ajnb3@cam.ac.uk>
2025-04-12 15:39:23 +0100

    Fix data download script

--- data/download_unzip_data.sh
+++ data/download_unzip_data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env/bash
 
-wget "https://www.arcgis.com/sharing/rest/content/items/6cb9092a37da4b5ea1b5f8b054c343aa/data"
+curl "https://www.arcgis.com/sharing/rest/content/items/6cb9092a37da4b5ea1b5f8b054c343aa/data" \
+    --output IPN_GB_2023.zip
 
 unzip "./IPN_GB_2023.zip"
